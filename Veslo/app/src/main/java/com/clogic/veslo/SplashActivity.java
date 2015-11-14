b0ef4615b7dc585f1c08b052ee5a94ad2993c240
CLogic <choiseungil29@gmail.com>
2015-11-14 16:52:45 +0900

    first commit

---
+++ Veslo/app/src/main/java/com/clogic/veslo/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.clogic.veslo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by clogic on 2015. 11. 14..
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
